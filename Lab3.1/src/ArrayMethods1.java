18aaaf6fbf229c45680e9da3f600015d3000e504
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2018-01-17 09:21:11 -0500

    ahahhahahahaha

---
+++ Lab3.1/src/ArrayMethods1.java
@@ -0,0 +1,39 @@
+
+public class ArrayMethods1 
+{
+	public static void bubbleSort(String[] list1)
+	{
+		
+	}
+	
+	public static void selectionSort(double[] list1)
+	{
+		
+	}
+	
+	public static void insertionSort(int[] list1)
+	{
+		
+	}
+	
+	public static void swap(int[]arr, int index1, int index2)
+	{
+		int temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+	
+	public static void swap(double[]arr, int index1, int index2)
+	{
+		double temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+	
+	public static void swap(String[]arr, int index1, int index2)
+	{
+		String temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+}
