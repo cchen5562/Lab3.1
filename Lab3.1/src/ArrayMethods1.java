808d42fde6c5ec708cb6dd8af9a75fced89470fd
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2018-01-18 09:22:27 -0500

    sdgws

--- Lab3.1/src/ArrayMethods1.java
+++ Lab3.1/src/ArrayMethods1.java
@@ -3,17 +3,41 @@ public class ArrayMethods1
 {
 	public static void bubbleSort(String[] list1)
 	{
-		
+		int m = 0;  //does m and n go inside or outside the loop?
+		int n = m + 1;
+		while(m > n)
+		{ 
+			swap(list1, m, n);
+		} m++;
 	}
 	
 	public static void selectionSort(double[] list1)
 	{
-		
+		int x = 0;
+		int y = x + 1;
+		while(x < y)
+		{ 
+			swap(list1, x, y);
+		} x++;
 	}
 	
 	public static void insertionSort(int[] list1)
 	{
-		
+		int i = 0;
+		int j = i + 1;
+	
+		while(j < list1.length)
+		{
+			if(i > j)
+			{
+				swap(list1, i, j);
+			}
+			
+			if(i < j)
+			{
+				j++;
+			}
+		}
 	}
 	
 	public static void swap(int[]arr, int index1, int index2)
